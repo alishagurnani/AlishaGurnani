ad2b532ebedd5b4f05de34afdae5b16ef1e18a77
alishagurnani <56456129+alishagurnani@users.noreply.github.com>
2021-08-11 00:02:07 -0400

    Update ACS_BLS_Crosswalk.R

--- Thesis/Code/ACS_BLS_Crosswalk.R
+++ Thesis/Code/ACS_BLS_Crosswalk.R
@@ -12,7 +12,7 @@ library(readxl)
 #BRING IN THE CROSSWALK
 column_names <- c("Sort_Order", "SOC_ID", "Occupation_Title", "ACS_CODE", "ACS_Occupation_Title")
 
-ACS_Crosswalk <- read_excel("/Users/agurnani/Documents/Spring Semester/Thesis/R Scripts /nem-occcode-acs-crosswalk.xlsx",
+ACS_Crosswalk <- read_excel("Thesis/R Scripts /nem-occcode-acs-crosswalk.xlsx",
                                 sheet= 1,
                                 skip = 5,
                                 col_names= column_names)
@@ -28,7 +28,7 @@ ACS_Crosswalk <- ACS_Crosswalk %>%
 #bring in BLS SOC 2010
 BLScolumn_names <- c("Occupation_Name", "SOC_ID", "Education", "Work_Experience", "Training")
 
-BLS_2010 <- read_excel("/Users/agurnani/Documents/Spring Semester/Thesis/Data/Education and Training Classifications 2010-present.xlsx",
+BLS_2010 <- read_excel("Thesis/Data/Education and Training Classifications 2010-present.xlsx",
                                   sheet= 1,
                                   skip = 2,
                                   col_names= BLScolumn_names)
@@ -44,7 +44,7 @@ AcsBls_Cross2010$OCCID<- str_remove(AcsBls_Cross2010$SOC_ID, "[-]")
 
 #ACS/BLS CSV for 2010
 
-write.csv(AcsBls_Cross2010, "/Users/agurnani/Documents/Spring Semester/Thesis/Data/Processed/ACS_BLS_2010.csv", row.names = FALSE)
+write.csv(AcsBls_Cross2010, "Thesis/Data/Processed/ACS_BLS_2010.csv", row.names = FALSE)
 
 
 
